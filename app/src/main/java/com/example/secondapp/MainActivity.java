adb73d4606b4a483c4fcbd84def4f0b83f876410
bek2rkanatbekov <Bekalone>
2022-03-29 20:22:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/secondapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.secondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
+import android.graphics.drawable.DrawableWrapper;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.style.BackgroundColorSpan;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    private Button enter;
+    private EditText email, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        enter = findViewById(R.id.Enter);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(email.getText().toString().equals("")){
+                    enter.setBackgroundColor(Color.GRAY);
+                } else {
+                    enter.setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(password.getText().toString().equals("")){
+                    enter.setBackgroundColor(Color.GRAY);
+                } else {
+                    enter.setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
+
+    }
+}
