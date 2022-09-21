8223ac6461ca716f448576bed9c41665b0836b1a
KANERnn <kanerkanatbekov@gmail.com>
2022-09-21 20:34:46 +0600

    up

--- app/src/main/java/com/example/secondapp/MainActivity.java
+++ app/src/main/java/com/example/secondapp/MainActivity.java
@@ -17,12 +17,15 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button enter;
     private EditText email, password;
+    private TextView textView, text1,text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +34,12 @@ public class MainActivity extends AppCompatActivity {
         enter = findViewById(R.id.Enter);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        textView = findViewById(R.id.text_view);
+        text1 = findViewById(R.id.text1);
+        text2 =  findViewById(R.id.text2);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
         email.addTextChangedListener(new TextWatcher() {
@@ -49,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(email.getText().toString().equals("")){
+                if (email.getText().toString().equals("")) {
                     enter.setBackgroundColor(Color.GRAY);
                 } else {
                     enter.setBackgroundColor(Color.YELLOW);
@@ -69,13 +75,28 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(password.getText().toString().equals("")){
+                if (password.getText().toString().equals("")) {
                     enter.setBackgroundColor(Color.GRAY);
                 } else {
                     enter.setBackgroundColor(Color.YELLOW);
                 }
             }
         });
-
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().equals("admin") | password.getText().toString().equals("admin")){
+                    Toast.makeText(MainActivity.this,"Вы успешно зарегистрировались!",Toast.LENGTH_SHORT).show();
+                    textView.setVisibility(View.GONE);
+                    text1.setVisibility(View.GONE);
+                    text2.setVisibility(View.GONE);
+                    email.setVisibility(View.GONE);
+                    password.setVisibility(View.GONE);
+                    enter.setVisibility(View.GONE);
+                } else{
+                    Toast.makeText(MainActivity.this,"Неправильный логин и пароль",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
